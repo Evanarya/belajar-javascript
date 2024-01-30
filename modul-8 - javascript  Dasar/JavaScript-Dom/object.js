d950fbf64ef3a7c3a0da0b54915dedde113696aa
Evan Arya <aevan354@gmail.com>
2024-01-30 08:53:41 +0700

    object json

--- modul-8 - javascript  Dasar/JavaScript-Dom/object.js
+++ modul-8 - javascript  Dasar/JavaScript-Dom/object.js
@@ -6,4 +6,14 @@ siswa.namaLengkap = function() {
     return this.namaDepan + " " + this.namaBelakang;
 }
 
+var siswa2 = {
+    namaDepan : " Romo",
+    namaBelakang : "aliando",
+    alamat : "bandung",
+    namaLengkap: function () {
+        return this.namaDepan + " " + this.namaBelakang;
+    },
+};
+
 alert("Nama :" + siswa.namaLengkap());
+alert("Nama :" + siswa2.namaLengkap());
